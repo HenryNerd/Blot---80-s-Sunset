c494b3c93e0ecd5d44ef27521cf0e7705a93ebe8
Henry <89225651+HenryNerd@users.noreply.github.com>
2024-08-24 19:24:29 -0500

    Add Triangles

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+//User Editable//
 var squareLength = 100;
 var squareHight = 30;
+var mountainHightMin = 5;
+var mountainHightMax = 10;
+var numberOfMountains = 7;
+
+
+//Not User Editable//
 var frequencyLength = 0;
+var frequencyHight = 0;
+var mountainfrequency = squareHight + (squareLength/numberOfMountains)
+var mountainHightMin = mountainHightMin + squareHight
+var mountainHightMax = mountainHightMax + squareHight;
+var currentHight = 0;
+
 frequencyLength = squareLength/10;
+frequencyHight = squareHight/3;
+mountainfrequency = squareLength / numberOfMountains
 
 drawLines([
     [[0,0], [0,squareHight]]
@@ -30,6 +45,60 @@ drawLines([
 drawLines([
     [[frequencyLength*3,0], [frequencyLength*3,squareHight]]
 ])
+
+drawLines([
+    [[frequencyLength*4,0], [frequencyLength*4,squareHight]]
+])
+
+drawLines([
+    [[frequencyLength*5,0], [frequencyLength*5,squareHight]]
+])
+
+drawLines([
+    [[frequencyLength*6,0], [frequencyLength*6,squareHight]]
+])
+
+drawLines([
+    [[frequencyLength*7,0], [frequencyLength*7,squareHight]]
+])
+
+drawLines([
+    [[frequencyLength*8,0], [frequencyLength*8,squareHight]]
+])
+drawLines([
+    [[frequencyLength*9,0], [frequencyLength*9,squareHight]]
+])
+
+drawLines([
+    [[frequencyLength*9,0], [frequencyLength*9,squareHight]]
+])
+
+drawLines([
+    [[0,frequencyHight], [squareLength,frequencyHight]]
+])
+
+drawLines([
+    [[0,frequencyHight*2], [squareLength,frequencyHight*2]]
+])
+
+currentHight = bt.randInRange(mountainHightMin, mountainHightMax)
+drawLines([
+    [[0,squareHight], [mountainfrequency,currentHight]]
+])
+
+drawLines([
+    [[mountainfrequency,currentHight], [mountainfrequency*2,squareHight]]
+])
+
+currentHight = bt.randInRange(mountainHightMin, mountainHightMax)
+drawLines([
+    [[mountainfrequency*2,squareHight], [mountainfrequency*2.75,currentHight]]
+])
+
+drawLines([
+    [[mountainfrequency*2.75,currentHight], [mountainfrequency*3.5,squareHight]]
+])
+
 
 drawLines([
     [[0,0], [0,0]]
